39ab8026e205bd65e69cd8fa4476ab416e6dcfaa
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-02 11:03:41 +0900

    :lipstick: スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,12 @@
 import { Links } from '../components/Links';
 import { Headline } from '../components/Headline';
+import classes from './Main.module.css'
 
 export function Main(props) {
   return (
-    <main>
+    <main className={classes.main}>
       <Headline page={props.page}>
-        <code>pages/{props.page}.js</code>
+        <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
       <Links />
     </main>
