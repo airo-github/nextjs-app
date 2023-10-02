47a97307da40032fb5c438bff0762ce69c58d81c
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-02 20:15:02 +0900

    :alembic: 試しにクリックイベントを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,23 @@ import styles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main';
 import { Header } from '../components/Header';
+import { useCallback } from 'react';
+
+// const handleClick = (e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(123);
+// }
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +28,13 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <a
+        href='/about'
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
 
       <Main page='index' />
 
