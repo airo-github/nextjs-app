a97433a19f10b6437edd1ed0e626720c6c5da133
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-02 20:55:58 +0900

    :sparkles: UseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,22 +3,18 @@ import styles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main';
 import { Header } from '../components/Header';
-import { useCallback } from 'react';
-
-// const handleClick = (e) => {
-//   console.log(e.target.href);
-//   e.preventDefault();
-//   alert(123);
-// }
+import { useCallback, useEffect } from 'react';
 
 export default function Home() {
-  const foo = 1;
+  useEffect(() => {
+    console.log('マウント時');
+    document.body.style.backgroundColor = 'lightblue';
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+    return () => {
+      console.log('アンマウント時');
+      document.body.style.backgroundColor = '';
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -28,13 +24,6 @@ export default function Home() {
       </Head>
 
       <Header />
-
-      <a
-        href='/about'
-        onClick={handleClick}
-      >
-        ボタン
-      </a>
 
       <Main page='index' />
 
