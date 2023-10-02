e8688deac655f3be5519474522c65d2da89dc668
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-02 21:35:25 +0900

    :sparkles: UseStateを使用してカウンターを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,25 @@ import styles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main';
 import { Header } from '../components/Header';
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
   useEffect(() => {
-    console.log('マウント時');
     document.body.style.backgroundColor = 'lightblue';
 
     return () => {
-      console.log('アンマウント時');
       document.body.style.backgroundColor = '';
     }
   }, [])
+
+  console.log(count);
 
   return (
     <div className={styles.container}>
@@ -24,6 +31,10 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <h1>{count}</h1>
+
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page='index' />
 
@@ -51,6 +62,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+    </div >
   );
 }
