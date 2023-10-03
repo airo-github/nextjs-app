0d97fe4f608546464136c5df7c575412580a59cd
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-03 11:27:34 +0900

    :zap: UseCallbackとuseEffectの解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,15 @@ import styles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main';
 import { Header } from '../components/Header';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10)
+      setCount((count) => count + 1);
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
@@ -20,8 +20,6 @@ export default function Home() {
       document.body.style.backgroundColor = '';
     }
   }, [])
-
-  console.log(count);
 
   return (
     <div className={styles.container}>
