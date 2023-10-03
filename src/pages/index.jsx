54c52fb477bf106a6a94d3b6fa4b6f98e1d8084e
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-03 14:54:17 +0900

    :zap: UseStateで文字列と真偽値について学習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,17 @@ import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState('');
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10)
       setCount((count) => count + 1);
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
@@ -20,6 +26,14 @@ export default function Home() {
       document.body.style.backgroundColor = '';
     }
   }, [])
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert('5文字以内にしてください');
+      return;
+    };
+    setText(e.target.value.trim());
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -30,9 +44,14 @@ export default function Home() {
 
       <Header />
 
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
 
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>
+        {isShow ? '非表示' : '表示'}
+      </button>
+
+      <input type='text' value={text} onChange={handleChange} />
 
       <Main page='index' />
 
